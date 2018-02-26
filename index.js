08bae2891ec1d4fc4618f3d26a5e12cb824a7ea3
ksha1018@naver.com <ksha1018@naver.com>
2018-02-26 12:57:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function happyHolidays() {
+  console.log('Happy holidays!')
+}
+
+function happyHolidaysTo(name) {
+  console.log('Happy holidays,' + name +'!')
+}
+
+function happyHolidayTo(holiday, name) {
+  console.log('Happy,' + holiday +' '+ name + '!')
+}
+
+function holidayCountdown(holiday, dy) {
+  console.log('It\'s ' + dy +  'days until ' + holiday + '!')
+}
+
+happyHolidays()
+happyHolidaysTo("David Kwon")
+happyHolidayTo("birthday", "David Kwon")
+holidayCountdown("birthday", 15)
